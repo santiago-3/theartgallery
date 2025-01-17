68895229a790d993bdd618ecade31e7275138211
Santiago González Rojo <santiagogonzalezrojo@yandex.ru>
2025-01-17 08:15:53 -0300

    Added admit cookie

--- app/middlewares/login.php
+++ app/middlewares/login.php
@@ -1,6 +1,7 @@
 <?php
 
+include_once('app/globals.php');
 $userAgent = strtolower($_SERVER['HTTP_USER_AGENT']);
-if (strpos($userAgent, 'linux') === false) { header('location: /'); }
-if (strpos($userAgent, 'firefox') === false) { header('location: /'); }
-if ($_SERVER['REMOTE_ADDR'] != '172.22.0.1') { header('location: /'); }
+if ($_COOKIE['admit'] != $env['ADMIT']) {
+    header('location: /');
+}
